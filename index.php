6ef07d31a5b4ce2580992596b6a77b02f492764c
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2024-03-30 10:11:46 +0500

    Darsdagi misollar

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web 2-amalyot</title>
+</head>
+<body>
+    <!-- <?php
+    $a=$_GET['a'];
+    if($a%400==0){
+        echo "Kabisa yili ".$a;x
+    }
+    else if($a%4==0 and $a%100!=0 and $a%400!=0){
+        echo "Kabisa yili".$a;
+    }
+    else{
+        echo "Kabisa yili emas".$a;
+    }
+    ?> -->
+
+    <!-- <?php
+    $a=$_GET['a'];
+    $b=$_GET['b'];
+    $ek=$a*$b;
+    while($a!=$b){
+        if($a>$b){
+            $a-=$b;
+        }
+            $b-=$a;
+    }
+    $ekk=$ek/$a;
+    ?>
+    <h2>EKUK <?= $ekk?></h2> -->
+
+    <!-- <?php
+    for($i=1;$i<=9;$i+=2){
+        for($j=1;$j<=9;$j+=2){
+            $c=$i*$j;
+            echo "$i * $j = $c <br>";
+        }
+    }
+    ?> -->
+</body>
+</html>

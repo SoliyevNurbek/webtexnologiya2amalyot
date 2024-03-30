117a46386d797a09391d821dbda5e414f69720a8
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2024-03-30 10:13:47 +0500

    Examples from the lesson

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- <?php
     $a=$_GET['a'];
     if($a%400==0){
-        echo "Kabisa yili ".$a;x
+        echo "Kabisa yili ".$a;
     }
     else if($a%4==0 and $a%100!=0 and $a%400!=0){
         echo "Kabisa yili".$a;
